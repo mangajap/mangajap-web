a2bf4f147bd1a96aa1f0559f0a039b5fdbb7e88e
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-02 00:17:52 +0200

    feat: implement people-save component

--- src/app/components/people/people-save/people-save.component.ts
+++ src/app/components/people/people-save/people-save.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { Router, ActivatedRoute } from '@angular/router';
+import People from 'src/app/models/people.model';
+import Base64 from 'src/app/utils/base64/base64';
 
 @Component({
   selector: 'app-people-save',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleSaveComponent implements OnInit {
 
-  constructor() { }
+  people: People = new People();
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle("Ajouter une personne | MangaJap");
+
+    this.route.params.subscribe(params => {
+      if (params.id) {
+        People.find(params.id).then(response => {
+          this.people = response.data;
+        });
+      }
+    });
+  }
+
+
+  onImageChanged(file: File | null) {
+    if (file) {
+      Base64.encode(file, (base64) => this.people.image = base64);
+    } else {
+      this.people.image = null;
+    }
+  }
+
+
+  submit() {
+    if (!this.people.exists()) {
+      this.people.create()
+        .then(response => this.people.id = response.data.id)
+        .then(() => this.router.navigate(['/people', this.people.id]))
+        .catch(err => console.error(err))
+    } else {
+      this.people.update()
+        .then(() => this.router.navigate(['/people', this.people.id]))
+        .catch(err => console.error(err))
+    }
   }
 
 }
