354807026ac16b3f24ac46ad535c4d532904b5fc
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-02 20:21:44 +0200

    feat(volume): create link to volume-save

--- src/app/components/manga/volume/volume.component.ts
+++ src/app/components/manga/volume/volume.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import Volume from 'src/app/models/volume.model';
 
 @Component({
   selector: 'app-volume',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VolumeComponent implements OnInit {
 
-  constructor() { }
+  volume = new Volume();
+
+  constructor(
+    private titleService: Title,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      Volume.find(params.id, { include: "manga" })
+        .then(response => this.volume = response.data)
+        .then(() => {
+          this.titleService.setTitle(`${this.volume.manga.title} - Tome ${this.volume.number} | MangaJap`)
+        })
+        .catch(() => this.router.navigate(['**'], { skipLocationChange: true }));
+    })
   }
-
 }
