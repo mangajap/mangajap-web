96ce00ace3f1f4e9dc840a27722e6ef8f92add89
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-02 20:22:18 +0200

    feat(volume-save): create form

--- src/app/components/manga/volume-save/volume-save.component.ts
+++ src/app/components/manga/volume-save/volume-save.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import Manga from 'src/app/models/manga.model';
+import Volume from 'src/app/models/volume.model';
+import Base64 from 'src/app/utils/base64/base64';
 
 @Component({
   selector: 'app-volume-save',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VolumeSaveComponent implements OnInit {
 
-  constructor() { }
+  volume = new Volume();
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      if (params.id) {
+        Volume.find(params.id, { include: "manga" })
+          .then((response) => this.volume = response.data)
+          .then(() => {
+            this.titleService.setTitle(`${this.volume.manga.title} - Tome ${this.volume.number} - Modification | MangaJap`)
+          });
+      } else {
+        Manga.find(params.mangaId)
+          .then((response) => this.volume.manga = response.data)
+          .then(() => {
+            this.titleService.setTitle(`${this.volume.manga.title} - Ajouter un tome | MangaJap`)
+            this.volume.number = this.volume.manga.volumeCount + 1;
+          })
+          .catch((err) => console.error(err));
+      }
+    });
   }
 
+
+  onImageChanged(file: File | null) {
+    if (file) {
+      Base64.encode(file, (base64) => this.volume.coverImage = base64);
+    } else {
+      this.volume.coverImage = null;
+    }
+  }
+
+
+  submit() {
+    if (!this.volume.exists()) {
+      this.volume.create()
+        .then(response => this.volume.id = response.data.id)
+        .then(() => this.router.navigate(['/manga', this.volume.manga.id, 'volume', this.volume.id]))
+        .catch(err => console.error(err))
+    } else {
+      this.volume.update()
+        .then(() => this.router.navigate(['/manga', this.volume.manga.id, 'volume', this.volume.id]))
+        .catch(err => console.error(err))
+    }
+  }
 }
