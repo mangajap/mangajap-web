3d0cc52815e55f93d57858695a5ba16996c96efa
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-03 19:37:26 +0200

    feat(season-save): create input form

--- src/app/components/anime/season-save/season-save.component.ts
+++ src/app/components/anime/season-save/season-save.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import Anime from 'src/app/models/anime.model';
+import Season from 'src/app/models/season.model';
 
 @Component({
   selector: 'app-season-save',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeasonSaveComponent implements OnInit {
 
-  constructor() { }
+  season = new Season();
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      if (params.id) {
+        Season.find(params.id, { include: "anime" })
+          .then((response) => this.season = response.data)
+          .then(() => {
+            this.titleService.setTitle(`${this.season.anime.title} - Saison ${this.season.number} - Modification | MangaJap`)
+          });
+      } else {
+        Anime.find(params.animeId)
+          .then((response) => this.season.anime = response.data)
+          .then(() => {
+            this.titleService.setTitle(`${this.season.anime.title} - Ajouter une saison | MangaJap`)
+            this.season.number = this.season.anime.seasonCount + 1;
+          })
+          .catch((err) => console.error(err));
+      }
+    });
   }
 
+
+  submit() {
+    if (!this.season.exists()) {
+      this.season.create()
+        .then(response => this.season.id = response.data.id)
+        .then(() => this.router.navigate(['/anime', this.season.anime.id, 'season', this.season.id]))
+        .catch(err => console.error(err))
+    } else {
+      this.season.update()
+        .then(() => this.router.navigate(['/anime', this.season.anime.id, 'season', this.season.id]))
+        .catch(err => console.error(err))
+    }
+  }
 }
