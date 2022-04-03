efae275b678450c6602523114b9dc197eab21247
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-03 19:39:39 +0200

    feat(episode): create link to episode-save

--- src/app/components/anime/episode/episode.component.ts
+++ src/app/components/anime/episode/episode.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import Episode from 'src/app/models/episode.model';
 
 @Component({
   selector: 'app-episode',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpisodeComponent implements OnInit {
 
-  constructor() { }
+  episode = new Episode();
+
+  constructor(
+    private titleService: Title,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      Episode.find(params.id, { include: "season.anime" })
+        .then(response => this.episode = response.data)
+        .then(() => {
+          this.titleService.setTitle(`${this.episode.season.anime.title} - S${this.episode.season.number}E${this.episode.number} | MangaJap`)
+        })
+        .catch(() => this.router.navigate(['**'], { skipLocationChange: true }));
+    })
   }
-
 }
