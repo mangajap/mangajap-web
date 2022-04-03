66194afe5d31f64e5e76f2aea05f8ac0362e59fd
Lory-Stan TANASI <stantanasi@gmail.com>
2022-04-03 19:36:33 +0200

    feat(season): create link to season-save

--- src/app/components/anime/season/season.component.ts
+++ src/app/components/anime/season/season.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, Router } from '@angular/router';
+import Season from 'src/app/models/season.model';
 
 @Component({
   selector: 'app-season',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SeasonComponent implements OnInit {
 
-  constructor() { }
+  season = new Season();
+
+  constructor(
+    private titleService: Title,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      Season.find(params.id, { include: "anime" })
+        .then(response => this.season = response.data)
+        .then(() => {
+          this.titleService.setTitle(`${this.season.anime.title} - Saison ${this.season.number} | MangaJap`)
+        })
+        .catch(() => this.router.navigate(['**'], { skipLocationChange: true }));
+    })
   }
-
 }
