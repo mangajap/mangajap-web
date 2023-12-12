95dc61905cd918cc5750ad0568cc5fe1e2fdde44
Lory-Stan TANASI <stantanasi@gmail.com>
2023-12-12 11:37:44 +0100

    feat: install and configure Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'mangajap',
+    appId: '1:765459541968:web:fd5acd1ab2ba4d4c1193d5',
+    storageBucket: 'mangajap.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBERviz4ObXOcBPCHiY8weoU_zdA8UNcIk',
+    authDomain: 'mangajap.firebaseapp.com',
+    messagingSenderId: '765459541968',
+    measurementId: 'G-P784KGM19T',
+  },
+};
